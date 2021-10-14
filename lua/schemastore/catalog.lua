87188244d9688e550fb86a438e24ca94b361acf0
Maddison Hellstrom <maddy@na.ai>
2021-10-14 00:29:59 -0700

    CI: Add comment to catalog.lua to force re-build

--- lua/schemastore/catalog.lua
+++ lua/schemastore/catalog.lua
@@ -25,6 +25,8 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 
+-- Comment for test purposes
+
 -- stylua: ignore start
 
 local M = {}
